7e58c0e6750eb81ea2b75287ee5eea38a035ec5e
James <james.rudrum@hyperisland.se>
2019-01-14 17:11:40 +0000

    added rubocop gem, test for exceeding maximum balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,14 @@
 class Oystercard
 
-  attr_reader :balance
+  attr_accessor :balance
 
   def initialize(balance = 0)
     @balance = balance
   end
 
   def top_up(amount)
+    fail "Exceeding balance limit" if balance + amount > 90
+
     @balance += amount
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,7 +2,7 @@ require "oystercard"
 
 describe Oystercard do
   let(:oc) { Oystercard.new }
-  #it { is_expected.to respond_to(:balance)}
+  # it { is_expected.to respond_to(:balance) }
 
   it "checks that when initialized balance is 0" do
     expect(oc.balance).to eq 0
@@ -13,7 +13,11 @@ describe Oystercard do
     it { is_expected.to respond_to(:top_up).with(1).argument }
 
     it "adds to balance" do
-      expect{oc.top_up(20)}.to change{oc.balance}.by(20)
+      expect { oc.top_up(20) }.to change { oc.balance }.by(20)
+    end
+
+    it "enforces a maximum balance" do
+      expect { oc.top_up(100) }.to raise_error "Exceeding balance limit"
     end
 
   end
