471847e2d49aa7d4ec246b3bc2f4d84f0f7e2caa
James <james.rudrum@hyperisland.se>
2019-01-14 16:54:08 +0000

    refactored step 4 tests

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,7 +2,7 @@ require "oystercard"
 
 describe Oystercard do
   let(:oc) { Oystercard.new }
-  it { is_expected.to respond_to(:balance)}
+  #it { is_expected.to respond_to(:balance)}
 
   it "checks that when initialized balance is 0" do
     expect(oc.balance).to eq 0
@@ -10,9 +10,10 @@ describe Oystercard do
 
   describe "#top_up" do
 
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
     it "adds to balance" do
-      oc.top_up(20)
-      expect(oc.balance).to eq 20
+      expect{oc.top_up(20)}.to change{oc.balance}.by(20)
     end
 
   end
