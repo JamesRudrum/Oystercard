354e082cd1a9492dd7a50487661133efd270bf3e
James <james.rudrum@hyperisland.se>
2019-01-14 16:35:17 +0000

    wrote failing test to check balance, passed test to check balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,9 @@
 require "oystercard"
 
 describe Oystercard do
+it { is_expected.to respond_to(:balance)}
+
+  it "checks that when initialized balance is 0" do
+    expect(subject.balance).to eq 0
+  end
 end
