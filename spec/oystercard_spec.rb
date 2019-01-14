37f9bc9d937a8f1192adba0c4a16ff095ec3555d
James <james.rudrum@hyperisland.se>
2019-01-14 16:40:42 +0000

    Failing test for new top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,9 +1,19 @@
 require "oystercard"
 
 describe Oystercard do
-it { is_expected.to respond_to(:balance)}
+  it { is_expected.to respond_to(:balance)}
 
   it "checks that when initialized balance is 0" do
     expect(subject.balance).to eq 0
+  end
+
+  describe "#top_up" do
+    let(:oc) { Oystercard.new }
+
+    it "adds to balance" do
+      oc.top_up(20)
+      expect(oc.balance).to eq 20
+    end
+
   end
 end
